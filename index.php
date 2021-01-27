595e671c1b2c48c9da9c6e833771199e18059685
casintec <levyfranco@outlook.com>
2021-01-27 16:33:10 -0300

    Configuração Admin Login System

--- index.php
+++ index.php
@@ -1,11 +1,12 @@
 <?php 
 
+session_start();
 require_once("vendor/autoload.php");
 
 use \Slim\Slim;
 use \Casintec\Page;
 use \Casintec\PageAdmin;
-
+use \Casintec\Model\User;
 
 $app = new Slim();
 
@@ -20,10 +21,40 @@ $app->get('/', function() {
 });
 
 $app->get('/admin', function() {
-    
+	
+	User::verifyLogin();
+	
 	$page = new PageAdmin();
 
 	$page->setTpl("index");
+
+});
+
+$app->get('/admin/login', function() {
+    
+	$page = new PageAdmin([
+		"header"=> false,
+		"footer"=> false
+	]);
+
+	$page->setTpl("login");
+
+});
+
+$app->post('/admin/login', function() {
+    
+	User::login($_POST["login"], $_POST["password"]);
+	header("Location: /admin");
+	exit;
+
+});
+
+$app->get('/admin/logout', function() {
+
+	User::logout();
+
+	header("Location: /admin/login");
+	exit;
 
 });
 
