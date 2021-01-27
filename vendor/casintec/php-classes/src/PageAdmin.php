f1477e41e83fe609bb1570a677e3fc6a582ddc3d
casintec <levyfranco@outlook.com>
2021-01-27 14:21:10 -0300

    Criação Configuração do Template PageAdmin

---
+++ vendor/casintec/php-classes/src/PageAdmin.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Casintec;
+
+class PageAdmin extends Page{
+
+    public function __CONSTRUCT($opts = array(), $tpl_dir = '/views/admin/'){
+
+        parent::__CONSTRUCT($opts, $tpl_dir);
+
+    }
+
+}
+
+?>
