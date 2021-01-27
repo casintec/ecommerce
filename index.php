d3740634d5e9043ab205c35628911b93d981ff83
casintec <levyfranco@outlook.com>
2021-01-27 11:25:59 -0300

    Configuração Template Page.php, header.html, index.html, footer.html

--- index.php
+++ index.php
@@ -2,17 +2,18 @@
 
 require_once("vendor/autoload.php");
 
-$app = new \Slim\Slim();
+use \Slim\Slim;
+use \Casintec\Page;
+
+$app = new Slim();
 
 $app->config('debug', true);
 
 $app->get('/', function() {
     
-	$Sql = new Casintec\DB\Sql();
+	$page = new Page();
 
-	$results = $Sql->select("SELECT * FROM tb_users");
-
-	echo json_encode($results);
+	$page->setTpl("index");
 
 });
 
