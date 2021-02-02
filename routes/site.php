a900bcc9b5b2fd676943debbb24d0d1b2ff42120
casintec <levyfranco@outlook.com>
2021-02-02 00:57:34 -0300

    Categorias Vs Produtos Aula 113

--- routes/site.php
+++ routes/site.php
@@ -2,6 +2,8 @@
 
 use \Casintec\Page;
 use \Casintec\Model\Product;
+use \Casintec\Model\Category;
+
 
 $app->get('/', function() {
 	
@@ -11,6 +13,22 @@ $app->get('/', function() {
 
 	$page->setTpl("index", [
 		'products'=>Product::checklist($products)
+	]);
+
+});
+
+
+$app->get('/category/:idcategory', function($idcategory){
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$page = new Page();
+
+	$page->setTpl("category", [
+		'category'=>$category->getValues(),
+		'products'=>Product::checkList($category->getProducts())
 	]);
 
 });
