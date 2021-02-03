45da24ff02eeb9d99d5d79cc3828f0a573347a31
casintec <levyfranco@outlook.com>
2021-02-03 20:39:10 -0300

    Site Login - Aula 119

--- routes/site.php
+++ routes/site.php
@@ -3,7 +3,9 @@
 use \Casintec\Page;
 use \Casintec\Model\Product;
 use \Casintec\Model\Category;
+use \Casintec\Model\User;
 use \Casintec\Model\Cart;
+use \Casintec\Model\Address;
 
 $app->get('/', function() {
 	
@@ -137,6 +139,59 @@ $app->post("/cart/freight", function(){
 	header("Location: /cart");
 	exit;
 
-})
+});
+
+$app->get("/checkout", function(){
+
+	User::verifyLogin(false);
+
+	$cart = Cart::getFromSession();
+
+	$address = new Address();
+
+	$page = new Page();
+
+	$page->setTpl("checkout", [
+		'cart'=>$cart->getValues(),
+		'address'=>$address->getValues()
+	]);
+
+});
+
+$app->get("/login", function(){
+
+	$page = new Page();
+
+	$page->setTpl("login", [
+		'error'=>User::getError()
+	]);
+
+});
+
+$app->post("/login", function(){
+
+	try{
+
+		User::login($_POST['login'], $_POST['password']);
+
+	}catch(Exception $e){
+
+		User::setError($e->getMessage());
+
+	}
+
+	header("Location: /checkout");
+	exit;
+
+});
+
+$app->get("/logout", function(){
+
+	User::logout();
+
+	header("Location: /login");
+	exit;
+
+});
 
 ?>
---
+++ vendor/casintec/php-classes/src/Model/Address.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Casintec\Model;
+
+use \Casintec\DB\Sql;
+use \Casintec\Model;
+
+class Address extends Model{
+
+
+
+}
+
+?>
